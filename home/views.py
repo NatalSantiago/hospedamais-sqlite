277e6a69e7c3cbc1012070a2237f4a97c23568e0
Natal Santiago <natal.santiago.filha@gmail.com>
2023-03-31 21:41:51 -0300

    Correção no template movimentoApartsItens_add que tava dando erro quando o usuario clicava em salvar sem informar a descrição do Item

--- home/views.py
+++ home/views.py
@@ -616,40 +616,6 @@ def itens_consumo_aparts_apartamento(request, apartamento_id):
     return render(request, 'home/movimentoAparts/listaItensConsumo.html', context)
 
 ################### Lançar novo item no apartamento ###################
-"""""
-@login_required
-def LancarNovoItemHospede(request):
-    form = InserirItensConsumoApartForm(request.POST or None)
-    try:
-        empresa = request.user.perfilusuario.empresa
-        nome_empresa = empresa.nome
-        apartID = request.GET.get('apartID')
-    except (PerfilUsuario.DoesNotExist):
-        return redirect('itens_consumo_aparts_apartamento')
-    context = {
-        'form': form,
-        'empresa': empresa,
-        'nome_empresa': nome_empresa,
-        'nomeApartamento': request.GET.get('nomeApartamento'),
-        'apartID': apartID,
-    }
-    if request.POST:
-        if form.is_valid():
-            itemconsumo = form.save(commit=False)
-            itemconsumo.empresa = empresa
-            itemconsumo.apartamento = apartID
-            itemconsumo.save()
-            return redirect('apartHome')
-    return render(request, 'home/movimentoAparts/movimentoApartsItens_add.html', context)
-
-
-from home.models import PerfilUsuario,hospedes,apartamentos,ItensConsumo,MovimentosAparts
-
-
-##############################################################################
-
-"""""
-from decimal import Decimal
 
 @login_required
 def LancarNovoItemHospede(request, movID):
@@ -701,68 +667,11 @@ def LancarNovoItemHospede(request, movID):
               return redirect(reverse('LancarNovoItemHospede', kwargs={'movID': movID}) + f"?nomeApartamento={nomeApartamento}&apartID={apartID}&movID={movID}")
           elif action == 'save_voltar':
               return redirect('itens_consumo_aparts_apartamento', apartID)
-          else:
-              return redirect('itens_consumo_aparts_apartamento', apartID)
+          elif action == 'save_exit':
+              return redirect('apartHome')
           
     return render(request, 'home/movimentoAparts/movimentoApartsItens_add.html', context)
 
-
-"""""
-
-from django.shortcuts import render, redirect
-from django.urls import reverse
-
-
-@login_required
-def LancarNovoItemHospede(request, movID):
-    form = InserirItensConsumoApartForm(request.POST or None)
-    empresa = request.user.perfilusuario.empresa
-    itens = ItensConsumo.objects.filter(empresa=empresa)
-    intensConsumoList = [item.descricao for item in itens]
-    nomeApartamento = request.GET.get('nomeApartamento')
-    apartID = request.GET.get('apartID')
-
-    context = {
-       'form': form,
-       'empresa': empresa,
-       'nomeApartamento': nomeApartamento,
-       'apartID': apartID,
-       'movID': movID,
-       'intensConsumoList': intensConsumoList
-       }
-
-    print("Antes de tentar val")
-    if form.is_valid():
-        item = form.save(commit=False)
-        preco_item = request.POST.get('precoItem')
-        qtd_lancamento = request.POST.get('qtdItem')
-        valor_total = request.POST.get('precoTotal')
-
-#        preco_item = form.cleaned_data['precoItem']
-#        qtd_lancamento = form.cleaned_data['qtdItem']
-#        valor_total = form.cleaned_data['precoTotal']
-        item = ItensConsumoAparts(
-            preco_item=preco_item,
-            qtd_lancamento=qtd_lancamento,
-            valor_total=valor_total,
-        )
-        print("Antes de tentar salvar")
-        item.save()
-        action = request.POST.get('action')
-        if action == 'save_add':
-            return redirect(reverse('LancarNovoItemHospede', kwargs={'movID': movID}) + f"?nomeApartamento={nomeApartamento}&apartID={apartID}&movID={movID}")
-        elif action == 'save_voltar':
-            return redirect('itens_consumo_aparts_apartamento', apartID)
-        else:
-            return redirect('itens_consumo_aparts_apartamento', apartID)
-    else:
-        # Exibir mensagens de erro
-        print("O formulário é inválido!")
-        print(form.errors)        
-
-    return render(request, 'home/movimentoAparts/movimentoApartsItens_add.html', context)
-
-"""""
 
 ################### Exclusão de Itens de Consumo ###################
 
